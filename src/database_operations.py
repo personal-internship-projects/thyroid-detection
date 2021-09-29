863dd77055cf13fc0560eb5d07dcdb7a06d9db25
Sreejith Subhash <sreejithsubhash198@gmail.com>
2021-09-29 15:40:32 +0530

    Added db insert and db delete function

--- src/database_operations.py
+++ src/database_operations.py
@@ -1,7 +1,9 @@
 import cassandra
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, QueryExhausted
 from cassandra.auth import PlainTextAuthProvider
 from src.logger.auto_logger import autolog
+from os import listdir
+import csv
 import json
 class CassandraOperations:
     def __init__(self):
@@ -11,13 +13,15 @@ class CassandraOperations:
         self.finalCsvTrain  =  "src/dataset/final_csv/train"
         self.goodCsvPath    =  "./src/dataset/csv_operation/GoodCSV"
         self.badCsvPath     =  "./src/dataset/csv_operation/BadCSV"
+        self.keyspace_name  =  "ineuron"
 
     
-    def schemaParser(self):
+    def schemaParser(self) -> dict():
         dic = dict()
         try:
             with open(self.schema_path, 'r') as f:
                 dic = json.load(f)
+            
                 f.close()
         except Exception as e:
             autolog("Error: " + str(e))
@@ -29,7 +33,7 @@ class CassandraOperations:
         try:
             autolog("Trying to initialize Connection...")
             cloud_config= {
-                'secure_connect_bundle': 'G:\\Ineuron\\secure-connect-test.zip'
+                'secure_connect_bundle': 'src/secure-connect-test.zip'
             }
             auth_provider = PlainTextAuthProvider('djMBOJUicLZEvpHTGZFRxDBI', 'WCYx-3FA+gBijXY.YqKWUbMnLh8Wg2bS5ZPuUU8ex4Hzlh6IhmZZbtT81ZAOxNYy_ld5HhT.D76SfBtSfph6ZMeXWZm50ozHjic2A-Dihriicj2nQcOe0.-,fKt1AfY4')
             cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
@@ -38,26 +42,46 @@ class CassandraOperations:
         except ConnectionError:
             autolog("Error while connecting to database.")
     
+    def deleteTable (self, table_name):
+        try:
+            self.session.execute(f"Drop table if exists {self.keyspace_name}.{table_name}")
+            autolog(f"Deleted {table_name} successfully")
+        except:
+            autolog(f"Failed to delete table {table_name}")
 
-
-    def createTable(self, keyspace_name, table_name):
+    def createTable(self, table_name):
         autolog("Function Started")
         column_dict = self.schemaParser()
         for column_name in column_dict.keys():
-            if column_name=='age':
-                continue
             datatype_of_columns = column_dict[column_name]
             try:
-                self.session.execute(f'ALTER TABLE {keyspace_name}.{table_name} ADD "{column_name}" {datatype_of_columns}')
-            except cassandra.InvalidRequest as e :
-                autolog(f"Column {column_name} already exists.")
+                self.session.execute(f'ALTER TABLE {self.keyspace_name}.{table_name} ADD "{column_name}" {datatype_of_columns}')
+                autolog(f"Adding column {column_name}")
             except Exception as e:
-                autolog("TABLE {table_name} does not exist.")
+                autolog(f"Column {column_name} already exists or TABLE {table_name} does not exist.")
                 try:
-                    self.session.execute(f"CREATE TABLE  {keyspace_name}.{table_name} (id int PRIMARY KEY, {column_name} {datatype_of_columns})")
+                    self.session.execute(f"CREATE TABLE  {self.keyspace_name}.{table_name} (id int PRIMARY KEY, {column_name} {datatype_of_columns})")
                     autolog(f"TABLE {table_name} created.")
                 except :
-                    autolog("Failed to create table {table_name}")
+                    autolog(f"Failed to create table {table_name}")
+
+
+    def insertValidatedData(self, path, table_name):
+        scheme_dict = self.schemaParser()
+        lst_dict = [x for x in scheme_dict]
+        lst = ' '.join([str(elem)+"," for elem in lst_dict])
+        lst = lst[:-1]
+        for files in listdir(path):
+            with open (f"{path}/{files}", "r") as csv_file:
+                next(csv_file)
+                reader = csv.reader(csv_file, delimiter="\n")
+                for rows in enumerate(reader):  
+                    for words in rows[1]:
+                        query = f"INSERT INTO {self.keyspace_name}.{table_name} ({lst}) VALUES ({words})"
+                        print(query)
+                        self.session.execute(query)
+
+    
                 
 
 
