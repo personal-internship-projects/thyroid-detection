999a406115fb8c771be7b2a6ba29cf413512eee5
Sreejith Subhash <sreejithsubhash198@gmail.com>
2021-10-08 16:01:44 +0530

    added parameters to functions

--- src/test.py
+++ src/test.py
@@ -2,9 +2,9 @@ import src.database_operations as dboc
 
 db = dboc.CassandraOperations()
 db.databaseConnection()
-db.createPreprocessedCsvDirectory()
+db.createPreprocessedCsvDirectory(db.combinedTest)
 db.deleteTable("test")
 db.createTable('test')
-db.insertValidatedData(db.finalCsvTest, "test")
-db.fetch(db.combinedTest,'test')
+db.insertValidatedData(db.finalCsvTest, "test", db.schemaPath)
+db.fetch(db.combinedTest,'test', db.schemaPath)
 
