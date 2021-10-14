8ee8af045b28d4c5e75fba011e9e4b20fb17decb
Sreejith Subhash <sreejithsubhash198@gmail.com>
2021-10-14 15:47:20 +0530

    added control flow for preprocessing.py

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,8 @@
 import src.Data_Validation as dv
 import src.database_operations as dboc 
+import src.preprocesing as prp
 
-
-zz =dv.DataValidation()
+zz = dv.DataValidation()
 zz.makeFinalCsvDirectory(zz.finalCsvPredict)
 z,g,dic = zz.verifyingSchema(zz.schemaPathPredict)
 a= zz.validateColumnLength(g, zz.predictCsvPath)
@@ -17,3 +17,14 @@ db.deleteTable('predict')
 db.createTable('predict', db.schemaPathPredict)
 db.insertValidatedData(db.finalCsvPredict, "predict", db.schemaPathPredict)
 db.fetch(db.combinedPredict, "predict", db.schemaPathPredict)
+
+pre = prp.Preprocessing()
+pre.createPreprocessedDirectory()
+pre.readCsv(pre.predictCsv)
+pre.dropUnnecessaryColumns()
+pre.replaceWithNan()
+pre.mappingCategoricalColumns()
+pre.getDummies()
+pre.labelEncoding()
+pre.exportCsv(pre.predictCsv)
+
