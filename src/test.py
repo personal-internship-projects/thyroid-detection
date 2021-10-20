42d2d5ca2b13638a41962bda6829cce25806b8c4
Sreejith Subhash <sreejithsubhash198@gmail.com>
2021-10-20 16:03:18 +0530

    Added file to load and unload pkl

--- src/test.py
+++ src/test.py
@@ -1,5 +1,8 @@
 import src.database_operations as dboc
+from src.model_operations import loadModel
 import src.preprocesing as prp
+from src.clustering import Kmeansclustering
+from src.logger.auto_logger import autolog
 
 db = dboc.CassandraOperations()
 db.databaseConnection()
@@ -14,7 +17,24 @@ pre.createPreprocessedDirectory()
 pre.readCsv(pre.testCsv)
 pre.dropUnnecessaryColumns()
 pre.replaceWithNan()
-pre.mappingCategoricalColumns()
-pre.getDummies()
-pre.labelEncoding()
-pre.exportCsv(pre.preprocessedTestCsv)
+pre.encodingCategoricalColumnsPrediction()
+pre.applyingLogTransformation()
+is_null_present = prp.isnullPresent(prp.dataframe,prp.preprocessedNullCsv)
+
+if (is_null_present):
+    prp.dataframe = prp.imputeNanvalues(prp.dataframe)
+
+X_test, Y_test = pre.seperateLabelfeature('class')
+
+K_Mean = loadModel("src/models/kmeans-clustering.pkl") 
+numberOfClusters = K_Mean.elbowplot(X_test)
+X_train_clusters = K_Mean.create_clusters(X_test, numberOfClusters)
+autolog(f"number of clusters are: {numberOfClusters}")
+
+dfTransport = X_train_clusters.join(Y_test)
+autolog("Starting export ...")
+pre.exportCsv(dfTransport, pre.preprocessedTrainCsv)
+
+
+
+
--- src/train.py
+++ src/train.py
@@ -1,7 +1,11 @@
+from numbers import Number
 import src.File_Type_Validation as fv
 import src.Data_Validation as dv
+from src.clustering import Kmeansclustering
 import src.database_operations as dboc
+from src.logger.auto_logger import autolog
 import src.preprocesing as prp
+
 x = fv.File_Type_Validation("./src/dataset")
 x.createCsvDir()
 x.convertToCsv()
@@ -22,12 +26,32 @@ db.createTable('train', db.schemaPath)
 db.insertValidatedData(db.finalCsvTrain, "train", db.schemaPath)
 db.fetch(db.combinedTrain, "train",  db.schemaPath)
 
+
 pre = prp.Preprocessing()
 pre.createPreprocessedDirectory()
 pre.readCsv(pre.trainCsv)
 pre.dropUnnecessaryColumns()
 pre.replaceWithNan()
-pre.mappingCategoricalColumns()
-pre.getDummies()
-pre.labelEncoding()
-pre.exportCsv(pre.preprocessedTrainCsv)
+pre.encodingCategoricalColumnsTraining()
+pre.applyingLogTransformation()
+isNullPresent = pre.isnullPresent(pre.dataframe,pre.preprocessedNullCsv)
+
+if (isNullPresent):
+    pre.dataframe = pre.imputeNanvalues(pre.dataframe)
+
+X_train, Y_train = pre.seperateLabelfeature('class')
+X_train, Y_train = pre.resampleData(X_train, Y_train)
+
+autolog("Clustering started.")
+K_Mean = Kmeansclustering() 
+numberOfClusters = K_Mean.elbowplot(X_train)
+
+X_train_clusters = K_Mean.create_clusters(X_train, numberOfClusters)
+autolog(f"number of clusters are: {numberOfClusters}")
+
+dfTransport = X_train_clusters.join(Y_train)
+autolog("Starting export ...")
+pre.exportCsv(dfTransport, pre.preprocessedTrainCsv)
+autolog("Done.")
+
+print(numberOfClusters)
