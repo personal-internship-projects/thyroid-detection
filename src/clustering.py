e0800dd6aa2401322ba9d9ca593ef95534bce45e
Govind Choudhary <govind2220000@gmail.com>
2021-11-05 15:44:40 +0530

    added docstring to clustering.py

--- src/clustering.py
+++ src/clustering.py
@@ -8,55 +8,88 @@ from sklearn.metrics import silhouette_score
 from src.logger.auto_logger import autolog
 import logging
 
+
 class Kmeansclustering:
 
     def __init__(self):
-        self.modelsDirs    = "src/models/kmeans-clustering.pkl"
+        self.modelsDirs = "src/models/kmeans-clustering.pkl"
         logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
+    def elbowplot(self, data):
+        """
+        This function plots the elbow plot to find the optimal number of clusters
 
-    def elbowplot(self,data):
+        Args:
+            data ([pandas.DataFrame]): [Stores all the feature columns of the data]
+
+        Returns:
+            [int]: [Returns the optimal number of clusters or knee value]
+        """
         self.wcss = []
-        self.n = range(2,15)
+        self.n = range(2, 15)
         for i in self.n:
             kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
             kmeans.fit(data)
             self.wcss.append(kmeans.inertia_)
-        plt.plot(self.n,self.wcss,marker="+") # creating the graph between WCSS and the number of clusters
+        # creating the graph between WCSS and the number of clusters
+        plt.plot(self.n, self.wcss, marker="+")
         plt.title('The Elbow Method')
         plt.xlabel('Number of clusters')
         plt.ylabel('WCSS')
         plt.savefig('src/dataset/preprocessed/K-Means_Elbow.PNG')
-        d = pd.DataFrame({'No.of_Clusters' : self.n, 'WSS' : self.wcss})
+        d = pd.DataFrame({'No.of_Clusters': self.n, 'WSS': self.wcss})
         # plt.scatter(x = "No.of_Clusters", y = 'WSS', data = d, marker="+")
         # plt.savefig('src/dataset/preprocessed/WSS_Elbow.PNG')
-        self.kn = KneeLocator(self.n, self.wcss, curve='convex', direction='decreasing', polynomial_degree=8)
+        self.kn = KneeLocator(self.n, self.wcss, curve='convex',
+                              direction='decreasing', polynomial_degree=8)
         print(self.wcss)
         print(self.kn.knee)
         return self.kn.knee
 
+    def silhoutee_scores(self, data):
+        """
+        This function calculates the silhouette score for the given data
 
-
-    def silhoutee_scores(self,data):
+        Args:
+            data ([pandas.DataFrame]): [Stores all the feature columns of the data]
+        """
         self.s_score = []
         #self.data = data
         #data.to_csv("ff.csv",index= None, header=True)
         #autolog("Clustering started")
         for i in self.n:
             #self.kmeans      = KMeans(n_clusters=i, init='k-means++', random_state=42)
-            #self.kmeans.fit(data)
-            labels=KMeans(n_clusters=i,init="k-means++",random_state=200).fit(data).labels_
-            print ("Silhouette score for k(clusters) = "+str(i)+" is "
-           +str(silhouette_score(data,labels,sample_size=5000,random_state=200)))
-            self.s_score.append(silhouette_score(data,labels,sample_size=5000,random_state=200))
+            # self.kmeans.fit(data)
+            labels = KMeans(n_clusters=i, init="k-means++",
+                            random_state=200).fit(data).labels_
+            print("Silhouette score for k(clusters) = "+str(i)+" is "
+                  + str(silhouette_score(data, labels, sample_size=5000, random_state=200)))
+            self.s_score.append(silhouette_score(
+                data, labels, sample_size=5000, random_state=200))
 
     def scores_clustering(self):
-        mycenters = pd.DataFrame({'No.of_Clusters' : self.n, 'WSS' : self.wcss, 'Silhouette_score': self.s_score})
-        mycenters.to_csv("scores.csv",index=None,header=True)
+        """
+        This function calculates the silhouette score for the given data and stores everythin into a CSV file
 
-    def create_clusters(self,data,number_of_clusters):
+        """
+        mycenters = pd.DataFrame(
+            {'No.of_Clusters': self.n, 'WSS': self.wcss, 'Silhouette_score': self.s_score})
+        mycenters.to_csv("scores.csv", index=None, header=True)
+
+    def create_clusters(self, data, number_of_clusters):
+        """
+        This function creates the clusters based on the elbow plot
+
+        Args:
+            data ([pandas.DataFrame]): [Stores all the feature columns of the data]  
+            number_of_clusters ([int]): [parameter value for k-means algorithm for creating clusters]
+
+        Returns:
+            [pandas.DataFrame]: [Stores the cluster labels for each row of the data]
+        """
         self.data = data
-        self.kmeans = KMeans(n_clusters=number_of_clusters,init='k-means++',random_state=42)
+        self.kmeans = KMeans(n_clusters=number_of_clusters,
+                             init='k-means++', random_state=42)
         self.kmeans.fit(data)
         self.y_kmeans = self.kmeans.predict(data)
         self.data['Cluster'] = self.y_kmeans
