4c02a4cc939ad34549ee40888107574631f36e6d
Govind Choudhary <govind2220000@gmail.com>
2021-11-05 16:25:56 +0530

    Added Docstring to ModelOPerations.py

--- src/model_operations.py
+++ src/model_operations.py
@@ -1,25 +1,40 @@
 import pickle
 from src.logger.auto_logger import autolog
 
+
 def saveModel(path, model):
+    """
+    This function saves a model to a file.
+
+    Args:
+        path ([string]): [The path to save the model to]
+        model ([pickle]): [The model to save]
+    """
     try:
         with open(path, 'wb') as file:
             pickle.dump(model, file)
         autolog(f"Model saved to {path}.")
-    
+
     except Exception as e:
         autolog("An exception occured while saving model.", 3)
 
 
 def loadModel(path):
+    """
+    This function loads a model from a file.
+
+    Args:
+        path ([string]): [Loads model from this path]
+
+    Returns:
+        [model]: [Variable of saved model]
+    """
     try:
         with open(path, 'rb'):
             model = pickle.load(path)
-        
+
         autolog(f"Model loaded successfully.")
         return model
 
     except Exception as e:
         autolog("An exception occured while loading model.", 3)
-
-
