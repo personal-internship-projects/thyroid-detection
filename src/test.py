7a58e91a0810685d6b2f500056b41be0eb787860
Sreejith Subhash <sreejithsubhash198@gmail.com>
2021-11-25 18:07:15 +0530

    added control flow and added default model prediction

--- src/test.py
+++ src/test.py
@@ -21,6 +21,7 @@ pre.encodingCategoricalColumnsTraining()
 is_null_present = pre.isnullPresent(pre.dataframe, pre.preprocessedNullCsvTest)
 
 X_test, Y_test = pre.seperateLabelfeature('class')
+predictedData = X_test
 
 if (is_null_present):
     X_test = pre.imputeNaNValuesOnTestAndPredict(X_test)
@@ -29,18 +30,42 @@ if (is_null_present):
 ## for applying quantile transformer
 
 autolog("Applying Logarithmic Transformer...")
-X_test= pre.LogTransformer(X_test)
+finalDataframeTest = pre.LogTransformer(X_test)
 autolog("Log Transformer applied")
 
 try:
     K_Mean = loadModel("src/models/kmeans-clustering.pkl") 
 
-    X_test["Cluster"] = K_Mean.predict(X_test)
+    finalDataframeTest["Cluster"] = K_Mean.predict(finalDataframeTest)
+    predictedData["Cluster"] = finalDataframeTest["Cluster"]
+    
 except Exception as e:
     autolog(f"An error occured {e}")
+
+
+# testing started
+autolog("Prediction started")
+clusterID = finalDataframeTest['Cluster'].unique()
+
+for id in clusterID:
+    autolog(f"Training started for Cluster {id}.")
+    ## Separating data based on cluster
+    clusterDataTest = finalDataframeTest[finalDataframeTest['Cluster'] == id]
     
-dfTransport = X_test.join(Y_test)
-pre.exportCsv(dfTransport, pre.preprocessedTestCsv)
+    # ## Prepare the feature and Label columns
+    x = clusterDataTest.drop(['Cluster'], axis=1)
+   
+    # ## Load the model      
+    pred_model = loadModel(f"{pre.predModelDirs}/{id}.pkl")
+    predictedData.loc[predictedData['Cluster'] == id, 'class'] = pred_model.predict(x)
+    
 
+decoder = loadModel("src/models/encoder.pkl")
 
+predictedData["class"] = decoder.inverse_transform(predictedData["class"].astype(int))
+predictedData.drop(['Cluster'], axis=1, inplace=True)
+
+autolog("Saving predicted data...")
+pre.exportCsv(predictedData, pre.preprocessedTestCsv)
+autolog("Data saved")
 
