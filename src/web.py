55832821128d1f3d14fa0f9ceda74e3bb1f7b0f2
Sreejith Subhash <sreejithsubhash198@gmail.com>
2022-01-10 19:54:39 +0530

    Changes to be committed: 	modified:   src/web.py

--- src/web.py
+++ src/web.py
@@ -4,48 +4,74 @@ from streamlit_autorefresh import st_autorefresh
 import os, time
 # File Processing Pkgs
 import pandas as pd
+from os.path import isfile 
+
+def lock():
+    if not isfile("access.lock"):
+        with open("access.lock", "w") as f:
+            f.write("locked")
+            
+def unlock():
+    if isfile("access.lock"):
+        os.remove("access.lock")
+        
+def islocked():
+    if isfile("access.lock"):
+        return True
+    return False
 
 def main():
     st.title("File Upload Tutorial")
     menu   = ["Train","Default Prediction","Custom File Prediction","About"]
     choice = st.sidebar.selectbox("Menu",menu)
     if choice == "Train":
-        st.subheader("Train")
-        if st.button("Train"):			
-            st.info("Training started.")
-            def load_logs_file():
-                dir_root = os.path.dirname(os.path.abspath(__file__))
-                logs_file = open(dir_root + '/src/logs/scraper.log', 'r')
-                return logs_file
-            st.write('Refresh to see logs.')
-            list_of_logs = ""
-            while True:     
-                z = st.empty()       
-                logs_data = load_logs_file()
-                time.sleep(3)
-                for line in logs_data:
-                    list_of_logs += line            
-                xz = z.code(list_of_logs)
-                st.write(xz)
-            st.success("Training Successful")
+        if islocked():
+            st.write("Prediction is under process. Try again after sometime.")
+        else:
+            lock()
+            st.subheader("Train")
+            if st.button("Train"):			
+                st.info("Training started.")
+                def open_file(file_path="src/logs/scraper.log"):
+                    while True:
+                        if not islocked():
+                            break
+                        with open(file_path, 'r') as f:
+                            return f.read()
+                
+                prediction()
+                
+                unlock()
             
+                
     elif choice == "Default Prediction":
-        st.subheader("Prediction (default)")
-        if st.button("Predict"):			
-            st.info("Prediction started.")
-            st.success("Prediction Successful")
+        if islocked():
+            st.write("Prediction is under process. Try again after sometime.")
+        else:
+            lock()
+            st.subheader("Prediction (default)")
+            if st.button("Predict"):			
+                st.info("Prediction started.")
+                st.success("Prediction Successful")
+            unlock()
             
+                
     elif choice == "Custom File Prediction":
         st.subheader("Prediction (custom)")
         data_file = st.file_uploader("Upload CSV",type=['csv'])
-
-        if st.button("Predict"):
-            if data_file is not None:			
-                st.info("Prediction started.")
-                df = pd.read_csv(data_file)
-                st.dataframe(df)
-                st.success("Prediction Successful")
-                
+        if islocked():
+            st.write("Prediction is under process.")
+        else:
+            lock()
+            if st.button("Predict"):
+                if data_file is not None:			
+                    st.info("Prediction started.")
+                    df = pd.read_csv(data_file)
+                    st.dataframe(df)
+                    st.success("Prediction Successful")
+            unlock()
+            
+                        
     elif choice == "About":
         st.subheader("About")
         st.write("This project demonstrates thyroid prediction using machine learning.")
