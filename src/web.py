df157b6acce621b8ee4f025e1b44c933bf1bfc55
Sreejith Subhash <sreejithsubhash198@gmail.com>
2022-01-15 15:46:58 +0530

    added flask

--- src/web.py
+++ src/web.py
@@ -1,81 +1,59 @@
-import streamlit as st
-import streamlit.components.v1 as stc
-from streamlit_autorefresh import st_autorefresh
-import os, time
-# File Processing Pkgs
-import pandas as pd
-from os.path import isfile 
+import flask
+from flask import render_template, request
+from os.path import isfile
+from src.actions import MLoperations 
+from src.logger.auto_logger import autolog
+import requests 
+import shutil
 
-def lock():
-    if not isfile("access.lock"):
-        with open("access.lock", "w") as f:
-            f.write("locked")
-            
-def unlock():
-    if isfile("access.lock"):
-        os.remove("access.lock")
-        
-def islocked():
-    if isfile("access.lock"):
-        return True
-    return False
+mlops = MLoperations()
+#flask application object
+app = flask.Flask(__name__)
 
-def main():
-    st.title("File Upload Tutorial")
-    menu   = ["Train","Default Prediction","Custom File Prediction","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
-    if choice == "Train":
-        if islocked():
-            st.write("Prediction is under process. Try again after sometime.")
-        else:
-            lock()
-            st.subheader("Train")
-            if st.button("Train"):			
-                st.info("Training started.")
-                def open_file(file_path="src/logs/scraper.log"):
-                    while True:
-                        if not islocked():
-                            break
-                        with open(file_path, 'r') as f:
-                            return f.read()
-                
-                prediction()
-                
-                unlock()
-            
-                
-    elif choice == "Default Prediction":
-        if islocked():
-            st.write("Prediction is under process. Try again after sometime.")
-        else:
-            lock()
-            st.subheader("Prediction (default)")
-            if st.button("Predict"):			
-                st.info("Prediction started.")
-                st.success("Prediction Successful")
-            unlock()
-            
-                
-    elif choice == "Custom File Prediction":
-        st.subheader("Prediction (custom)")
-        data_file = st.file_uploader("Upload CSV",type=['csv'])
-        if islocked():
-            st.write("Prediction is under process.")
-        else:
-            lock()
-            if st.button("Predict"):
-                if data_file is not None:			
-                    st.info("Prediction started.")
-                    df = pd.read_csv(data_file)
-                    st.dataframe(df)
-                    st.success("Prediction Successful")
-            unlock()
-            
-                        
-    elif choice == "About":
-        st.subheader("About")
-        st.write("This project demonstrates thyroid prediction using machine learning.")
-        st.write("Developed by: Govind Choudhary and Sreejith Subhash")
-                
-if __name__ == '__main__':
-	main()
+@app.route('/', methods=["GET"])
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/test', methods=["POST"])
+def start_test():
+    mlops.test_model()
+
+def upload_data(data):
+
+    autolog("inside upload_data function")
+    global count
+    url = "https://api.anonfiles.com/upload"
+    header = {'Content-type': 'application/octet-stream'}
+    files = {
+        'file':("src/dataset/preprocessed/predict/preprocessed.csv", open("src/dataset/preprocessed/predict/preprocessed.csv", "rb"))
+    }
+    try:
+        tmp = requests.post('https://api.anonfiles.com/upload', files=files)
+        tmp = tmp.json()
+        autolog(f"tmp: {tmp}")
+        url = tmp["data"]["file"]["url"]["full"]
+        autolog("Uploaded data successfully")
+        return 0, f"Uploaded data successfully. <a href={url}>Download link</a>"
+    except Exception as e:
+        autolog(f"Failed to upload data: {e}")
+        return 1, "Failed to upload data"
+    finally:
+        autolog("closed file")        
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    if request.method == "POST":
+            data = request.files['file']
+            # convert the data to a dictionary
+            data.save("src/dataset/csv_operation/PredictCSV/datasetpredict.csv")
+            # convert the dictionary to a string
+            mlops.prediction()
+            code, msg = upload_data("datasetpredict")
+            shutil.rmtree("src/dataset/csv_operation/PredictCSV")
+            return msg
+           
+
+
+app.run(debug=True)
